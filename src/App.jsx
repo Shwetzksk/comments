64fb35b351c33e89ec69e4dae139a7433da34827
Shwetaksk <shweta.kumari@zybisys.com>
2024-07-29 00:08:19 +0530

    Create post & post card components

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,77 @@ import { FiEye } from "react-icons/fi";
 import { IoCloseSharp } from "react-icons/io5";
 
 import { twMerge } from "tailwind-merge";
+import { BiDotsHorizontalRounded } from "react-icons/bi";
+import { RxChatBubble } from "react-icons/rx";
 
 function App() {
   return (
     <>
-      <Signup />
+      <CreatePost />
+      <PostCard />
+      <PostCard isEdited={true} />
     </>
   );
 }
 
+function CreatePost() {
+  return (
+    <div className="bg-[#27292D] border-2 border-[#35373B] rounded-lg py-6 px-5  flex flex-col gap-y-4">
+      <header>
+        <h3 className="text-[#C5C7CA] text-lg font-medium text-left">
+          Create Post
+        </h3>
+      </header>
+      <main className="bg-[#191920] flex items-center rounded-lg p-4 gap-x-4 h-fit">
+        <div className="rounded-full bg-[#27292D] flex items-center justify-center w-12 h-12">
+          💬
+        </div>
+        <textarea
+          placeholder="How are you feeling today?"
+          rows={1}
+          className="resize-none w-full placeholder:text-[#7F8084] text-base focus:outline-none flex items-center h-full"
+        />
+      </main>
+      <footer className="flex justify-end">
+        <Button>Post</Button>
+      </footer>
+    </div>
+  );
+}
+function PostCard({ isEdited }) {
+  return (
+    <div className="bg-[#27292D] border-2 border-[#35373B] rounded-lg py-6 px-5  flex flex-col gap-y-4">
+      <header className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <div className="w-11 h-11 rounded-full bg-slate-400"></div>
+          <div className="flex flex-col text-left">
+            <h3 className="text-[#C5C7CA] text-lg font-medium ">
+              Theresa Webb
+            </h3>
+            <p className="text-sm font-medium text-[#7F8084] ">
+              8mins ago {Boolean(isEdited) && <span>• Edited</span>}
+            </p>
+          </div>
+        </div>
+        <BiDotsHorizontalRounded className="text-xl text-[#C5C7CA]" />
+      </header>
+      <main className="bg-[#191920] flex items-center rounded-lg p-4 gap-x-4 h-fit">
+        <div className="rounded-full bg-[#27292D] flex items-center justify-center w-12 h-12 text-lg">
+          👋
+        </div>
+        <p className="flex-1 text-left text-[#7F8084]">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit. Exercitation
+          veniam consequat sunt nostrud amet.
+        </p>
+      </main>
+      <footer className="flex justify-start items-center gap-2">
+        <RxChatBubble className="text-xl text-[#C5C7CA]" />
+        <p className="text-[#7F8084] text-sm font-medium">24 comments</p>
+      </footer>
+    </div>
+  );
+}
 function Signup() {
   return (
     <Card closeIcon={true}>

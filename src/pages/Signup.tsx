f44c8d11a6db1b80637a01bfdb28eae9076ae3a6
Shwetaksk <shweta.kumari@zybisys.com>
2024-07-29 16:28:44 +0530

    SVG fix

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,10 +1,10 @@
-import logo from "@/assets/logo.svg";
 import Form from "@/features/SignupForm";
+import Logo from "@/assets/logo.svg";
 
 export default function Signup() {
   return (
     <section className="flex flex-col items-center justify-center  h-full">
-      <img src={logo} alt="logo" className="w-10 h-10 mb-12" />
+      <img src={Logo} alt="logo" className="w-10 h-10 mb-12" />
       <Form isModal={false} />
     </section>
   );

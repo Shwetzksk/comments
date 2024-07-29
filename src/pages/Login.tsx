43e08ea255eae661e55c824889d41d4de1bef43a
Shwetaksk <shweta.kumari@zybisys.com>
2024-07-29 17:13:01 +0530

    logo fix

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,9 @@
 import Form from "@/features/LoginForm";
-import Logo from "@/assets/logo.svg";
 
 export default function Login() {
   return (
     <section className="flex flex-col items-center justify-center  h-full">
-      <img src={Logo} alt="logo" className="w-10 h-10 mb-12" />
+      <img src="./logo.svg" alt="logo" className="w-10 h-10 mb-12" />
       <Form isModal={false} />
     </section>
   );
